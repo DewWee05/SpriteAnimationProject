4b95645892945c1217dbc464fe48b3ca961f9daa
DewWee05 <68929628+DewWee05@users.noreply.github.com>
2021-01-13 19:56:16 -0800

    Initial commit

---
+++ Juggernaut.js
@@ -0,0 +1,25 @@
+class Juggernaut {
+    constructor(game, x, y, spritesheet) {
+        Object.assign(this, {game, x, y, spritesheet});
+        this.spritesheet = ASSET_MANAGER.getAsset("./juggernaut_sprite.png");
+
+        this.size = 0;
+        this.facing = 0;
+        this.state = 0;
+
+        this.velocity = 0;
+
+        this.animation = new Animator(this.spritesheet, 12, 269, 16, 32, 4, 0.5, 14, false, true);
+    };
+
+    loadAnimations(spritesheet);
+
+    update() {
+
+    };
+
+    draw(ctx) {
+
+        this.animation.drawFrame(this.game.clockTick, ctx, 0, 0, 3);
+    }
+}
